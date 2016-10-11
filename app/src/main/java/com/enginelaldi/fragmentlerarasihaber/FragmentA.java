20f0d908524c2ce9a415033ea226d601c51f69cb
enginelaldi <enginelaldi@hotmail.com>
2016-10-11 15:47:47 +0300

    Revert "yeni yorum"
    This reverts commit 550aaf607637fd2b50e12946c9ffea06161cf492.

--- app/src/main/java/com/enginelaldi/fragmentlerarasihaber/FragmentA.java
+++ app/src/main/java/com/enginelaldi/fragmentlerarasihaber/FragmentA.java
@@ -40,7 +40,6 @@ public class FragmentA extends Fragment implements View.OnClickListener{
 
     }
     //burada bir yorum var
-    //bi yorum daha
 
     @Override
     public void onClick(View view) {
